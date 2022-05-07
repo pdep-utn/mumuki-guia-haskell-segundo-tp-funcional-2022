2e277b56dc0c73b84da037c54fd0ff29ea9fce03
licfleischman.af@gmail.com <licfleischman.af@gmail.com>
2022-05-07 14:25:17 +0000

    Mumuki Export on 2022-05-07 14:25:17 +0000

--- 00002_Ejecución de un programa/test.hs
+++ 00002_Ejecución de un programa/test.hs
@@ -1,4 +1,10 @@
-describe "Descripción del grupo" $ do 
-  
-  it "Descripción del ejemplo" $ do
-    True `shouldBe` True
+ describe "Test Punto 2 - Tests de NOP" $ do
+        --it "NOP incrementa el program counter" $ do
+        --  ((programCounter . nop) xt8088) `shouldBe` 1
+        
+        it "NOP no cambia el acumulador" $ do
+            ((acumuladorA . nop) xt8088) `shouldBe` 0
+      
+        --it "Programa con 3 NOP avanza 3 veces el program counter" $ do
+        --    ((programCounter . nop . nop . nop) xt8088) `shouldBe` 3
+        
